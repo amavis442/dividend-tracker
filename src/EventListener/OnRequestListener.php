2d8347889202ed6e2c32ff42c1cc107fb4bd38d0
Patrick Teunissen <patrick.teunissen@crossmedia-club.nl>
2020-02-01 15:53:41 +0000

    Revert "update to symfony 4.4"
    This reverts commit c424cde12f02176714df52f10e52cd8918f342ce

--- src/EventListener/OnRequestListener.php
+++ src/EventListener/OnRequestListener.php
@@ -3,8 +3,8 @@
 namespace App\EventListener;
 
 use Doctrine\ORM\EntityManager;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\UsageTrackingTokenStorage;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use App\Entity\User;
 
 class OnRequestListener
@@ -12,13 +12,13 @@ class OnRequestListener
     private $tokenStorage;
     private $em;
 
-    public function __construct(EntityManager $em, UsageTrackingTokenStorage $tokenStorage)
+    public function __construct(EntityManager $em, TokenStorage $tokenStorage)
     {
         $this->em = $em;
         $this->tokenStorage = $tokenStorage;
     }
 
-    public function onKernelRequest(RequestEvent $event)
+    public function onKernelRequest(GetResponseEvent $event)
     {
         if($this->tokenStorage->getToken()) {
             $user = $this->tokenStorage->getToken()->getUser();
