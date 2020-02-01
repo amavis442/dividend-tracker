33ddb5d960fdad7bc0d54254b7915222cf219534
Patrick Teunissen <patrick@banpagi.com>
2020-02-01 16:52:16 +0100

    Depredecated cleanup

--- src/EventListener/OnRequestListener.php
+++ src/EventListener/OnRequestListener.php
@@ -3,8 +3,8 @@
 namespace App\EventListener;
 
 use Doctrine\ORM\EntityManager;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\UsageTrackingTokenStorage;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use App\Entity\User;
 
 class OnRequestListener
@@ -12,13 +12,13 @@ class OnRequestListener
     private $tokenStorage;
     private $em;
 
-    public function __construct(EntityManager $em, TokenStorage $tokenStorage)
+    public function __construct(EntityManager $em, UsageTrackingTokenStorage $tokenStorage)
     {
         $this->em = $em;
         $this->tokenStorage = $tokenStorage;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelRequest(RequestEvent $event)
     {
         if($this->tokenStorage->getToken()) {
             $user = $this->tokenStorage->getToken()->getUser();
