2b50fb7d25fb9b5e4fd1cdfe93e33ac1cf06ebda
Patrick Teunissen <patrick@banpagi.com>
2020-02-01 16:57:30 +0100

    Deprecated fixes

--- src/EventListener/OnRequestListener.php
+++ src/EventListener/OnRequestListener.php
@@ -3,27 +3,27 @@
 namespace App\EventListener;
 
 use Doctrine\ORM\EntityManager;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\UsageTrackingTokenStorage;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use App\Entity\User;
 
 class OnRequestListener
 {
     private $tokenStorage;
-    private $em;
+    private $manager;
 
-    public function __construct(EntityManager $em, TokenStorage $tokenStorage)
+    public function __construct(EntityManager $manager, UsageTrackingTokenStorage $tokenStorage)
     {
-        $this->em = $em;
+        $this->manager = $manager;
         $this->tokenStorage = $tokenStorage;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelRequest(RequestEvent $event)
     {
         if($this->tokenStorage->getToken()) {
             $user = $this->tokenStorage->getToken()->getUser();
             if ($user instanceof User){
-                $filter = $this->em->getFilters()->enable('user_filter');
+                $filter = $this->manager->getFilters()->enable('user_filter');
                 $filter->setParameter('userID', $user->getId());
             }
         }
