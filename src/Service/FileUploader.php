812a287c6d81a369c9e58f359a48b373e66c779e
Patrick Teunissen <p.teunissen18@gmail.com>
2021-03-23 16:12:48 +0100

    Fix upload files

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -3,38 +3,34 @@ namespace App\Service;
 
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class FileUploader
 {
     private $targetDirectory;
-    private $file;
+    private $slugger;
 
-    public function __construct(string $targetDirectory)
+    public function __construct(string $targetDirectory, SluggerInterface $slugger)
     {
         $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
     }
 
     public function upload(UploadedFile $file)
     {
         $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
 
         try {
             $file->move($this->getTargetDirectory(), $fileName);
         } catch (FileException $e) {
-            // ... handle exception if something happens during file upload
+            throw $e;
         }
 
-        $this->file = $file;
         return $fileName;
     }
-    
-    public function getFile()
-    {
-        return $this->file;
-    } 
-
 
     public function getTargetDirectory()
     {
