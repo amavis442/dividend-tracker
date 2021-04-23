8fb338932ceb7340a744a8dba27c59c2592e965f
Patrick Teunissen <p.teunissen18@gmail.com>
2021-04-23 21:54:36 +0200

    Added dividend date service

---
+++ src/Command/DividendDateCommand.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Calendar;
+use App\Repository\CalendarRepository;
+use App\Repository\CurrencyRepository;
+use App\Repository\TickerRepository;
+use App\Service\DividendDateService;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DividendDateCommand extends Command
+{
+    protected static $defaultName = 'app:dividenddate';
+    protected static $defaultDescription = 'Get dividend data';
+    /**
+     * Get dividend info from external site
+     *
+     * @var DividendDateService
+     */
+    protected $dividendDateService;
+    /**
+     * getAll active tickers
+     *
+     * @var TickerRepository
+     */
+    protected $tickerRepository;
+    /**
+     * Dividend calendar
+     *
+     * @var CalendarRepository
+     */
+    protected $calendarRepository;
+    /**
+     * Currency
+     *
+     * @var CurrencyRepository
+     */
+    protected $currencyRepository;
+/**
+ * Entity manager
+ *
+ * @var EntityManagerInterface
+ */
+    protected $entityManager;
+
+
+
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TickerRepository $tickerRepository, 
+        CalendarRepository $calendarRepository, 
+        CurrencyRepository $currencyRepository,
+        DividendDateService $dividendDateService
+    ) {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+        $this->tickerRepository = $tickerRepository;
+        $this->dividendDateService = $dividendDateService;
+        $this->calendarRepository = $calendarRepository;
+        $this->currencyRepository = $currencyRepository;
+ 
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+            /* ->addArgument('ticker', InputArgument::OPTIONAL, 'Symbol of stock (MSFT, APPL)')
+               ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+         */
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $io = new SymfonyStyle($input, $output);
+        //$symbol = $input->getArgument('ticker');
+        $tickers = $this->tickerRepository->getActive();
+        $defaultCurrency = $this->currencyRepository->findOneBy(['symbol' => 'USD']);
+        $addedDates = 0;
+        foreach ($tickers as $ticker) {
+            $data = $this->dividendDateService->getUpcommingDividendInfo($ticker->getSymbol());
+            if (!$data) {
+                $io->info('No dividend data for '.$ticker->getFullname());
+                continue;
+            }
+            $io->info('Processing data for ticker: '.$ticker->getFullname());
+
+            foreach ($data as $payment)
+            {
+                $exDate = new DateTime($payment['ExDate']);               
+                $calendar = $this->calendarRepository->findOneBy(['ticker' => $ticker, 'exDividendDate' => $exDate]);
+                
+                if (!$calendar) {
+                    $currencySymbol = $payment['Currency'];
+                    $currency = $this->currencyRepository->findOneBy(['symbol' => $currencySymbol]);
+                    if (!$currency) {
+                        $currency = $defaultCurrency;
+                    }
+
+                    $payDate = new DateTime($payment['PayDate']);
+                    $recordDate = new DateTime($payment['RecordDate']);
+
+                    $calendar = new Calendar();
+                    $calendar
+                    ->setTicker($ticker)
+                    ->setCashAmount($payment['DividendAmount'])
+                    ->setExDividendDate($exDate)
+                    ->setPaymentDate($payDate)
+                    ->setRecordDate($recordDate)
+                    ->setCurrency($currency)
+                    ;
+                    $this->entityManager->persist($calendar);
+                    $this->entityManager->flush();
+
+                    $addedDates++;
+                }
+
+                /*
+                "DividendAmount" => 0.56
+                "Currency" => "USD"
+                "ExDate" => "2021-05-19"
+                "PayDate" => "2021-06-10"
+                "RecordDate" => "2021-05-20"
+                "DeclaredDate" => "2021-03-16"
+                "PaymentFrequency" => "Quarterly"
+                "Type" => "OrdinaryDividend"
+                */
+            }
+        }
+        $io->success('Done.... added '. $addedDates);
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/DividendDateService.php
@@ -0,0 +1,95 @@
+<?php
+namespace App\Service;
+
+use DateTime;
+use RuntimeException;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DividendDateService
+{
+    public const TOKEN_URL = "https://seekingalpha.com/market_data/xignite_token";
+    public const URL = "https://globalhistorical.xignite.com/";
+    /**
+     * Hold sthe returned dividend data
+     *
+     * @var array
+     */
+    private $dividenData;
+
+    /**
+     * Http client
+     *
+     * @var HttpClientInterface
+     */
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getToken()
+    {
+        $response = $this->client->request('GET', self::TOKEN_URL);
+        if ($response->getStatusCode() !== 200) {
+            throw new RuntimeException('Can not get token...');
+        }
+
+        return $response->toArray();
+    }
+
+    public function getStockHistory(array $token, string $ticker)
+    {
+        $url = "https://globalhistorical.xignite.com/v3/xGlobalHistorical.json/GetCashDividendHistory?"
+            . "IdentifierType=Symbol&Identifier=" . $ticker
+            . "&StartDate=01/01/" . (date('Y') - 3) . "&EndDate=12/30/" . (date('Y') + 3) . "&"
+            . "IdentifierAsOfDate=&CorporateActionsAdjusted=true&_token="
+            . $token['_token'] . "&_token_userid=" . $token['_token_userid'];
+
+        $response = $this->client->request('GET', $url);
+        if ($response->getStatusCode() !== 200) {
+            throw new RuntimeException('Can not get token...');
+        }
+        $data = $response->toArray();
+        if ($data['Outcome'] == 'Success') {
+            $this->dividenData = $data['CashDividends'];
+            return $data;
+        }
+    }
+
+    private function getTickerNextPayments(array $tickerData): array
+    {
+        $payments = [];
+        $currentDate = new DateTime();
+        $dividends = $tickerData['CashDividends'];
+        foreach ($dividends as $dividend) {
+            $payDate = new DateTime($dividend['PayDate']);
+            if ($payDate > $currentDate) {
+                $payments[] = $dividend;
+            }
+        }
+        return $payments;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $ticker
+     * @return void
+     */
+    public function getUpcommingDividendInfo(string $ticker): ?array
+    {
+        $tickerNextPayments = [];
+
+        $token = $this->getToken();
+        $tickerData = [];
+        $tickerData = $this->getStockHistory($token, $ticker);
+        if ($tickerData === null || count($tickerData['CashDividends']) === 0) {
+            return null;
+        }
+
+        $tickerNextPayments = $this->getTickerNextPayments($tickerData);
+
+        return $tickerNextPayments;
+    }
+}
