b76543cb14ae1324ea466e64e0fed01ddac01ff7
Patrick Teunissen <p.teunissen18@gmail.com>
2021-04-28 19:35:00 +0200

    Added number of effected positions

---
+++ src/Twig/UcFirstExtension.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace  App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+use Twig\Markup;
+
+
+class UcFirstExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [new TwigFilter('ucfirst', [$this, 'ucFirstFilter'])];
+    }
+
+    public function ucFirstFilter(string $val) : Markup
+    {
+        return new Markup(ucfirst($val), 'UTF-8');
+    }
+}
