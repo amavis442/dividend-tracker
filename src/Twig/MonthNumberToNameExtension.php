5705e4f78c043553f9f6f1f9d90c0211438f626f
Patrick Teunissen <p.teunissen18@gmail.com>
2021-04-28 19:13:56 +0200

    Added twig extension

---
+++ src/Twig/MonthNumberToNameExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace App\Twig;
+
+use DateTime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Class TypeCastingExtension
+ */
+class MonthNumberToNameExtension extends AbstractExtension
+{
+    /**
+     * @return array|\Twig_Filter[]
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('monthToName', function ($value) {
+                $rawDate = date('Y').'-'.$value.'-01';
+                $formatDate = new DateTime($rawDate);
+                
+                return $formatDate->format('F');
+            }),
+        ];
+    }
+}
