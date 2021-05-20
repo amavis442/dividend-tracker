7cb3104c69abffc80d61f2d2a560762eb929760b
Patrick Teunissen <p.teunissen18@gmail.com>
2021-05-20 16:36:26 +0200

    Added dividend feeds

--- src/Service/DividendDate/ISharesService.php
+++ src/Service/DividendDate/ISharesService.php
@@ -2,15 +2,18 @@
 
 namespace App\Service\DividendDate;
 
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Contracts\Service\DividendDatePluginInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ISharesService implements DividendDatePluginInterface
 {
     public const SEMB_FEED = 'https://www.blackrock.com/nl/particuliere-beleggers/produkten/251824/ishares-jp-morgan-emerging-markets-bond-ucits-etf/1495092399598.ajax?tab=distributions&fileType=json&subtab=table';
     public const ISPA_FEED = 'https://www.ishares.com/nl/particuliere-belegger/nl/producten/251973/ishares-stoxx-global-select-dividend-100-ucits-etf-de-fund/1497735778843.ajax?tab=distributions&fileType=json&subtab=table';
-
-
+    public const INRG_SEED = 'https://www.ishares.com/nl/particuliere-belegger/nl/producten/251911/ishares-global-clean-energy-ucits-etf/1497735778843.ajax?tab=distributions&fileType=json&subtab=table';
+    public const IPRP_SEED = 'https://www.ishares.com/nl/particuliere-belegger/nl/producten/251808/ishares-european-property-yield-ucits-etf/1497735778843.ajax?tab=distributions&fileType=json&subtab=table';
+    public const ISF_SEED = 'https://www.ishares.com/nl/particuliere-belegger/nl/producten/251795/ishares-ftse-100-ucits-etf-inc-fund/1497735778843.ajax?tab=distributions&fileType=json&subtab=table';
+    public const IUS3_SEED = 'https://www.ishares.com/nl/particuliere-belegger/nl/producten/251920/ishares-sp-smallcap-600-ucits-etf/1497735778843.ajax?tab=distributions&fileType=json&subtab=table';
+    public const IWDP_SEED = 'https://www.ishares.com/nl/particuliere-belegger/nl/producten/251920/ishares-sp-smallcap-600-ucits-etf/1497735778843.ajax?tab=chart&subtab=total-returns&thumbnail=Y';
 
     /**
      * Http client
@@ -33,8 +36,28 @@ class ISharesService implements DividendDatePluginInterface
                 $url = self::SEMB_FEED;
                 break;
             case 'ISPA':
-                $url = self:: ISPA_FEED;
+                $url = self::ISPA_FEED;
                 $currency = 'EUR';
+                break;
+            case 'INRG':
+                $url = self::INRG_SEED;
+                $currency = 'USD';
+                break;
+            case 'IPRP':
+                $url = self::IPRP_SEED;
+                $currency = 'EUR';
+                break;
+            case 'ISF':
+                $url = self::ISF_SEED;
+                $currency = 'GB';
+                break;
+            case 'IUS3':
+                $url = self::IUS3_SEED;
+                $currency = 'USD';
+                break;
+            case 'IWDP':
+                $url = self::IWDP_SEED;
+                $currency = 'USD';
                 break;
         }
 
@@ -48,7 +71,7 @@ class ISharesService implements DividendDatePluginInterface
         /*
         $bom = pack('H*','EFBBBF');
         $content = preg_replace("/^$bom/", '', $content);
-        */
+         */
         $data = json_decode($content, true);
         $row = $data['table']['aaData'];
         $item = [];
