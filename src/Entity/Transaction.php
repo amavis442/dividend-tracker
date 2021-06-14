18f238f8b76055c64a7baa0d148cc93f2ba39c52
Patrick Teunissen <p.teunissen18@gmail.com>
2021-06-14 13:58:08 +0200

    Order view net order value added

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -480,4 +480,9 @@ class Transaction
 
         return $this;
     }
+
+    public function netOrderValue() : float
+    {
+        return $this->getAllocation() + $this->getFinraFee() + $this->getStampduty() + $this->getFxFee() + $this->getTransactionFee();
+    }
 }
