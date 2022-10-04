faaf15701d6267bf0113e582fe5fc9912acb81f2
Patrick Teunissen <p.teunissen18@gmail.com>
2022-10-04 10:05:07 +0200

    Fix endpoint vuejs

--- assets/vue/index.js
+++ assets/vue/index.js
@@ -7,7 +7,7 @@ let baseURL;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost/';
 } else {
-  baseURL = 'https://dividend.banpagi.com/';
+  baseURL = 'https://dividend.odroid/';
 }
 Vue.prototype.$apiBaseUrl = baseURL;
 
