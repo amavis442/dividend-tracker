2c561c2f8a1387f3b5d08a728b4ecc025809d1d4
Patrick Teunissen <p.teunissen18@gmail.com>
2022-11-29 16:52:24 +0100

    Tooltip to tippy

--- assets/vue/index.js
+++ assets/vue/index.js
@@ -7,7 +7,7 @@ let baseURL;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost/';
 } else {
-  baseURL = 'https://dividend.odroid/';
+  baseURL = 'https://dividend.banpagi.com/';
 }
 
 /*
