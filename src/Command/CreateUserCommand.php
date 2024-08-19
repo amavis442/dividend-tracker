0c94417f719053c11d4b7470b15ca40bb61f3f14
Patrick Teunissen <p.teunissen18@gmail.com>
2024-08-19 13:53:51 +0200

    Check for correct helper

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -9,15 +9,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name:'app:create-user',
-    description:'Create a new user',
+    name: 'app:create-user',
+    description: 'Create a new user',
 )]
 class CreateUserCommand extends Command
 {
-    private $userManager;
+    private UserManager $userManager;
 
     public function __construct(UserManager $userManager)
     {
@@ -30,6 +31,9 @@ class CreateUserCommand extends Command
         $userManager = $this->userManager;
 
         $helper = $this->getHelper('question');
+        if (!$helper instanceof QuestionHelper) {
+            throw new \RuntimeException('Helper is not of type QuestionHelper');
+        }
         $questionEmail = new Question('Please enter the email: ', null);
         $questionEmail->setNormalizer(function ($value) {
             // $value can be null here
