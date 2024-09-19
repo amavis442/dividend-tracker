cd352d80591d0d29f87cef5c72c75047d34ad33d
Patrick Teunissen <p.teunissen18@gmail.com>
2024-09-19 22:29:27 +0000

    Yarn ci

--- ci/php_test_run.sh
+++ ci/php_test_run.sh
@@ -2,4 +2,8 @@ php bin/console cache:clear --env=test
 php bin/console doctrine:database:drop --if-exists --force --env=test
 php bin/console doctrine:database:create --env=test
 php bin/console doctrine:schema:update --complete --force --env=test
-php bin/phpunit
+# You can check last exit code with > echo $?
+# grep -q exits with 0 if value is found.
+#php vendor/bin/phpunit --testdox --debug --stop-on-failure --stop-on-error | grep -q 'OK' | echo $?
+php vendor/bin/phpunit --testdox --debug --stop-on-failure --stop-on-error
+echo $?
