45de78832a4eb8877ee8b9beff4c5f10d9dca6ae
Patrick Teunissen <p.teunissen18@gmail.com>
2024-09-24 20:05:23 +0200

    Added access tags and formatting

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,38 +2,44 @@
 
 namespace App\Controller;
 
-use ApiPlatform\Api\IriConverterInterface;
-use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use \Symfony\Component\HttpFoundation\Response;
-use \Symfony\Component\Routing\Annotation\Route;
-use \Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use ApiPlatform\Api\IriConverterInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     #[Route('/logout', name: 'app_logout')]
-    function logout(): void
+    public function logout(): void
     {
-        throw new \RuntimeException('This method can be blank - it will be intercepted by the logout key on your firewall');
+        throw new \RuntimeException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall'
+        );
     }
 
     #[Route('/login/json_form', name: 'app_login_json')]
-    public function index(NormalizerInterface $normalizer, #[CurrentUser] ?User $user = null): Response
-    {
+    public function index(
+        NormalizerInterface $normalizer,
+        #[CurrentUser] ?User $user = null
+    ): Response {
         return $this->render('security/login_json.html.twig', [
             'userData' => $normalizer->normalize($user, 'jsonld', [
                 'groups' => ['user:read'],
@@ -41,14 +47,25 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route('/login/json_result', name: 'app_login_json_result', methods: ['POST'])]
-    public function loginJson(IriConverterInterface  $iriConverter, #[CurrentUser] ?User $user = null): Response
-    {
-
+    #[
+        Route(
+            '/login/json_result',
+            name: 'app_login_json_result',
+            methods: ['POST']
+        )
+    ]
+    public function loginJson(
+        IriConverterInterface $iriConverter,
+        #[CurrentUser] ?User $user = null
+    ): Response {
         if (!$user) {
-            return $this->json([
-                'error' => 'Invalid login request: check that the Content-Type header is "application/json".',
-            ], 401);
+            return $this->json(
+                [
+                    'error' =>
+                        'Invalid login request: check that the Content-Type header is "application/json".',
+                ],
+                401
+            );
         }
 
         //$token = '...'; // somehow create an API token for $user
