cd25606c75911729ce581cfd181593bb19c22ed7
Patrick Teunissen <p.teunissen18@gmail.com>
2024-09-29 14:52:24 +0200

    Changed tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,12 @@
 module.exports = {
   content: [
     "./assets/**/*.js",
-    "./assets/**/*.vue",
     "./templates/**/*.html.twig",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [],
   prefix: 'tw-',
-  corePlugins: {
-    preflight: false,
-  },
   important: true,
 }
