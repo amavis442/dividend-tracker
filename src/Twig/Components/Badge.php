c8391c2ebfd49cff19540445f7b96039578a67d4
Patrick Teunissen <p.teunissen18@gmail.com>
2024-10-12 23:15:18 +0200

    Badge less orangy

--- src/Twig/Components/Badge.php
+++ src/Twig/Components/Badge.php
@@ -18,7 +18,7 @@ final class Badge
             'primary' => 'text-blue-900 bg-blue-300 hover:bg-blue-300',
             'secondary' => 'text-white bg-slate-500 hover:bg-slate-700',
             'danger' => 'text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none',
-            'info' =>  'text-white bg-orange-500 hover:bg-orange-700',
+            'info' =>  'text-white bg-orange-400 hover:bg-orange-400',
             default => throw new \LogicException(sprintf('Unknown badge variant "%s"', $this->variant)),
         };
     }
