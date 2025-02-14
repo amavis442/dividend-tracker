8c60ee795c80c71df826a696609771a9fe00e342
Patrick Teunissen <p.teunissen18@gmail.com>
2025-02-14 16:28:05 +0100

    Cleanup from bootstrap css

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,4 @@
 import './bootstrap.js';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-//import 'summernote/dist/summernote-bs5.css';
 import './styles/app.css';
-
-//import * as bootstrap from 'bootstrap';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import './js/Tooltip.js';
-//import bsCustomFileInput from 'bs-custom-file-input';
 import './js/turbo/turbo-helper.js';
-
-//bsCustomFileInput.init();
-//window.bootstrap = bootstrap;
-
