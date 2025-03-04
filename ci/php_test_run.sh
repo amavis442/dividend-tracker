becc522dfb291732cc63c568152e5945515af766
Patrick Teunissen <p.teunissen18@gmail.com>
2025-03-04 20:54:21 +0100

    ci unneeded option removed

--- ci/php_test_run.sh
+++ ci/php_test_run.sh
@@ -1,7 +1,7 @@
 php bin/console cache:clear --env=test
 php bin/console doctrine:database:drop --if-exists --force --env=test
 php bin/console doctrine:database:create --env=test
-php bin/console doctrine:schema:update --complete --force --env=test
+php bin/console doctrine:schema:update --force --env=test
 # You can check last exit code with > echo $?
 # grep -q exits with 0 if value is found.
 #php vendor/bin/phpunit --testdox --debug --stop-on-failure --stop-on-error | grep -q 'OK' | echo $?
