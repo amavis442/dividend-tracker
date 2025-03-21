08c89f9a116d9db9761ffc935ab21c9fb420bf9e
Patrick Teunissen <p.teunissen18@gmail.com>
2025-03-21 20:09:41 +0100

    Added more options for ckeditor

--- assets/controllers/ckeditor_controller.js
+++ assets/controllers/ckeditor_controller.js
@@ -1,16 +1,56 @@
 import { Controller } from '@hotwired/stimulus';
-import { ClassicEditor, Essentials, Bold, Italic, Font, Paragraph, Highlight } from 'ckeditor5';
+import {
+    ClassicEditor,
+    Essentials,
+    Bold,
+    Italic,
+    Font,
+    Paragraph,
+    Highlight,
+    Link,
+    Heading,
+    Table,
+    Image,
+    ImageCaption,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+} from 'ckeditor5';
+
+const LICENSE_KEY = 'GPL';
 
 export default class extends Controller {
     connect() {
         if (document.getElementsByClassName('ckeditor5').length > 0) {
             ClassicEditor
                 .create(document.querySelector('.ckeditor5'), {
-                    plugins: [Essentials, Bold, Italic, Font, Paragraph, Highlight],
-                    toolbar: [
-                        'undo', 'redo', '|', 'bold', 'italic', '|',
-                        'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight'
-                    ]
+                    licenseKey: LICENSE_KEY,
+                    plugins: [
+                        Heading,
+                        Essentials,
+                        Bold,
+                        Italic,
+                        Font,
+                        Paragraph,
+                        Highlight,
+                        Link,
+                        Table,
+                        Image,
+                        ImageCaption,
+                        ImageResize,
+                        ImageStyle,
+                        ImageToolbar,
+                        ImageUpload,
+                    ],
+                    toolbar: {
+                        shouldNotGroupWhenFull: true,
+                        items: [
+                            'undo', 'redo', '|', 'heading', '|', 'bold', 'italic', '|',
+                            'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|', 'link', '|', 'highlight',
+                            'insertImage', 'insertTable'
+                        ]
+                    },
                 })
                 .then(editor => {
                     console.log(editor);
