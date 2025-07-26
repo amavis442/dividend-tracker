adb8ca40842f487443696357cdad264476578738
Patrick Teunissen <p.teunissen18@gmail.com>
2025-07-26 22:35:49 +0200

    Deprecated some functions

--- src/Service/DividendServiceInterface.php
+++ src/Service/DividendServiceInterface.php
@@ -9,15 +9,40 @@ use Doctrine\Common\Collections\Collection;
 
 interface DividendServiceInterface
 {
-    public function getExchangeAndTax(Position $position, Calendar $calendar): array;
-    public function getPositionSize(Collection $transactions, Calendar $calendar): ?float;
-    public function getRegularCalendar(Ticker $ticker): ?Calendar;
-    public function getPositionAmount(Calendar $calendar): ?float;
-    public function getNetDividend(Position $position, Calendar $calendar): ?float;
-    public function getTotalNetDividend(Calendar $calendar): ?float;
-    public function getCashAmount(Ticker $ticker): ?float;
-    public function getForwardNetDividend(Ticker $ticker, float $amount): ?float;
-    public function getForwardNetDividendYield(Position $position, Ticker $ticker, float $amount, float $allocation): ?float;
-    public function getNetDividendPerShare(?Position $position): ?float;
-    public function setCummulateDividendAmount(bool $cummulateDividendAmount = true): DividendServiceInterface;
+	/**
+	 * @deprecated Use App\Service\ExchangeAndTaxResolverInterface
+	 */
+	public function getExchangeAndTax(
+		Position $position,
+		Calendar $calendar
+	): array;
+	/**
+	 * @deprecated Use App\Service\ShareEligibilityCalculator::calculate(Collection $transactions, Calendar $calendar): float
+	 */
+	public function getPositionSize(
+		Collection $transactions,
+		Calendar $calendar
+	): ?float;
+	public function getRegularCalendar(Ticker $ticker): ?Calendar;
+	public function getPositionAmount(Calendar $calendar): ?float;
+	public function getNetDividend(
+		Position $position,
+		Calendar $calendar
+	): ?float;
+	public function getTotalNetDividend(Calendar $calendar): ?float;
+	public function getCashAmount(Ticker $ticker): ?float;
+	public function getForwardNetDividend(
+		Ticker $ticker,
+		float $amount
+	): ?float;
+	public function getForwardNetDividendYield(
+		Position $position,
+		Ticker $ticker,
+		float $amount,
+		float $allocation
+	): ?float;
+	public function getNetDividendPerShare(?Position $position): ?float;
+	public function setCummulateDividendAmount(
+		bool $cummulateDividendAmount = true
+	): DividendServiceInterface;
 }
