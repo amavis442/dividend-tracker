0c9258a995acbe5c3fd88feae8a0621cf76e88b7
Patrick Teunissen <p.teunissen18@gmail.com>
2025-07-27 19:19:26 +0200

    Formatting

--- src/Serializer/AdminGroupsContextBuilder.php
+++ src/Serializer/AdminGroupsContextBuilder.php
@@ -1,32 +1,49 @@
 <?php
-// api/src/Serializer/BookContextBuilder.php
+
+// src/Serializer/AdminGroupsContextBuilder.php
+
 namespace App\Serializer;
 
-use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+use ApiPlatform\State\SerializerContextBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use App\Entity\Ticker;
 
-final class AdminGroupsContextBuilder implements SerializerContextBuilderInterface
+final class AdminGroupsContextBuilder implements
+	SerializerContextBuilderInterface
 {
-  private $decorated;
-  private $authorizationChecker;
+	private $decorated;
+	private $authorizationChecker;
 
-  public function __construct(SerializerContextBuilderInterface $decorated, AuthorizationCheckerInterface $authorizationChecker)
-  {
-    $this->decorated = $decorated;
-    $this->authorizationChecker = $authorizationChecker;
-  }
+	public function __construct(
+		SerializerContextBuilderInterface $decorated,
+		AuthorizationCheckerInterface $authorizationChecker
+	) {
+		$this->decorated = $decorated;
+		$this->authorizationChecker = $authorizationChecker;
+	}
 
-  public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array
-  {
-    $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
-    $resourceClass = $context['resource_class'] ?? null;
+	public function createFromRequest(
+		Request $request,
+		bool $normalization,
+		?array $extractedAttributes = null
+	): array {
+		$context = $this->decorated->createFromRequest(
+			$request,
+			$normalization,
+			$extractedAttributes
+		);
+		$resourceClass = $context['resource_class'] ?? null;
 
-    if ($resourceClass === Ticker::class && isset($context['groups']) && $this->authorizationChecker->isGranted('ROLE_ADMIN') && false === $normalization) {
-      $context['groups'][] = 'admin:input';
-    }
+		if (
+			$resourceClass === Ticker::class &&
+			isset($context['groups']) &&
+			$this->authorizationChecker->isGranted('ROLE_ADMIN') &&
+			false === $normalization
+		) {
+			$context['groups'][] = 'admin:input';
+		}
 
-    return $context;
-  }
+		return $context;
+	}
 }
