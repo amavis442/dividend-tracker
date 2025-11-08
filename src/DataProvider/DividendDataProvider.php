6137a94dfd2923ca9025c62a7aa6c5ad1578a8e5
Patrick Teunissen <p.teunissen18@gmail.com>
2025-11-08 11:27:30 +0100

    Exception handling.

--- src/DataProvider/DividendDataProvider.php
+++ src/DataProvider/DividendDataProvider.php
@@ -5,11 +5,13 @@ namespace App\DataProvider;
 use App\Repository\DividendCalendarRepository;
 use App\Entity\Ticker;
 use App\Entity\Calendar;
+use Psr\Log\LoggerInterface;
 
 class DividendDataProvider
 {
 	public function __construct(
-		private DividendCalendarRepository $diviRepo
+		private DividendCalendarRepository $diviRepo,
+		private LoggerInterface $logger
 	) {
 	}
 
@@ -22,7 +24,19 @@ class DividendDataProvider
 	 */
 	public function load(array $tickers, ?\DateTime $afterDate = null, ?\DateTime $beforeDate = null, array $types = [Calendar::REGULAR]): array
 	{
-		$ids = array_map(fn(Ticker $t) => $t->getId(), $tickers);
+		$logger = $this->logger;
+		try{
+		$ids = array_map(function(?Ticker $t) use ($logger) {
+			if (!isset($t) || $t == null) {
+				$logger->error('DividendDataProvider::load()#'.__LINE__.' Ticker() is null');
+			}
+			return  $t->getId();
+		}, $tickers);
+		} catch(\Exception $e) {
+			$logger->error($e->getMessage());
+			$logger->error(print_r($tickers, true));
+			throw $e;
+		}
 
 		return $this->mapByTicker(
 				$this->diviRepo->findByTickerIds($ids, $afterDate, $beforeDate, $types)
